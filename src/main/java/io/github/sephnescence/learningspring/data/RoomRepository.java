fe344b294c72e8c9986b7a35cbc6747edc018099
Blake Taylor <sephnescence@gmail.com>
2023-06-16 08:51:59 +1000

    Create RoomRepository. Since we're extending CrudRepository we get all the crud methods we need without defining them. We just need to define the Entity and class type of its ID. So Room and Long in this case

---
+++ src/main/java/io/github/sephnescence/learningspring/data/RoomRepository.java
@@ -0,0 +1,12 @@
+package io.github.sephnescence.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * We're only putting the @Repository annotation here to be absolutely clear that this is a repository
+ * It is not required to have
+ */
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> { // You pass in the Entity class and the ID class
+}
