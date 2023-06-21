b48a2219ae1a93903c0ec57705d8b0bba86e0c00
Blake Taylor <sephnescence@gmail.com>
2023-06-21 13:43:20 +1000

    Add the GET controller action for the guests and rooms endpoint

--- src/main/java/io/github/sephnescence/learningspring/webservice/WebserviceController.java
+++ src/main/java/io/github/sephnescence/learningspring/webservice/WebserviceController.java
@@ -2,6 +2,10 @@ package io.github.sephnescence.learningspring.webservice;
 
 import io.github.sephnescence.learningspring.business.ReservationService;
 import io.github.sephnescence.learningspring.business.RoomReservation;
+import io.github.sephnescence.learningspring.data.Guest;
+import io.github.sephnescence.learningspring.data.GuestRepository;
+import io.github.sephnescence.learningspring.data.Room;
+import io.github.sephnescence.learningspring.data.RoomRepository;
 import io.github.sephnescence.learningspring.util.DateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +18,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class WebserviceController {
     private final DateUtils dateUtils;
+    private final GuestRepository guestRepository;
     private final ReservationService reservationService;
+    private final RoomRepository roomRepository;
 
-    public WebserviceController(DateUtils dateUtils, ReservationService reservationService) {
+    public WebserviceController(DateUtils dateUtils, GuestRepository guestRepository, ReservationService reservationService, RoomRepository roomRepository) {
         this.dateUtils = dateUtils;
+        this.guestRepository = guestRepository;
         this.reservationService = reservationService;
+        this.roomRepository = roomRepository;
     }
 
     @RequestMapping(path = "/reservations")
@@ -28,5 +36,15 @@ public class WebserviceController {
     ) {
         Date date = dateUtils.createDateFromDateString(dateString);
         return reservationService.getRoomReservationsForDate(date);
+    }
+
+    @RequestMapping(path = "/guests")
+    public List<Guest> guests() {
+        return (List<Guest>) guestRepository.findAll();
+    }
+
+    @RequestMapping(path = "/rooms")
+    public List<Room> rooms() {
+        return (List<Room>) roomRepository.findAll();
     }
 }
